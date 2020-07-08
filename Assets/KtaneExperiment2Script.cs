a9549af78432fa57f7269971953583fe4459922b
Kavinkul <kavinkul@hotmail.co.uk>
2020-07-08 11:37:31 -0600

    Animation Experiment

---
+++ Assets/KtaneExperiment2Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KtaneExperiment2Script : MonoBehaviour  
+{
+	public Animator DoorAnimator;
+	public KMSelectable ActivatorCube;
+
+	void Start ()
+	{
+		ActivatorCube.OnInteract += delegate
+		{
+			Animation();
+			return false;
+		};
+	}
+
+	private void Animation()
+    {
+		AnimatorStateInfo currentAnimationState = DoorAnimator.GetCurrentAnimatorStateInfo(0);
+		if (currentAnimationState.IsName("Door Closed"))
+			DoorAnimator.Play("Door Opening");
+		else if (currentAnimationState.IsName("Door Opened"))
+			DoorAnimator.Play("Door Closing");
+	}
+}
