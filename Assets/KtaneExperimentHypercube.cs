291ed6fae9e647353db391f875112ef02d1c9521
Kavinkul <kavinkul@hotmail.co.uk>
2020-07-20 16:52:56 -0600

    Update Hypercube Experiment

--- Assets/KtaneExperimentHypercube.cs
+++ Assets/KtaneExperimentHypercube.cs
@@ -6,95 +6,111 @@ using Experiment4;
 
 public class KtaneExperimentHypercube : MonoBehaviour
 {
-    public GameObject ModuleObject;
+    public KMSelectable ActivatorCube;
     public GameObject CylinderObject;
+    public Transform HypercubeTransform;
 
-    private GameObject[] _testObject;
-    private GameObject[] _edgeObjects;
-    private static readonly Point4D[] _vertices = new Point4D[16]
-    {
-        new Point4D(-1, -1, -1, -1),
-        new Point4D(-1, -1, -1, 1),
-        new Point4D(-1, -1, 1, -1),
-        new Point4D(-1, 1, -1, -1),
-        new Point4D(1, -1, -1, -1),
-        new Point4D(-1, -1, 1, 1),
-        new Point4D(-1, 1, -1, 1),
-        new Point4D(1, -1, -1, 1),
-        new Point4D(-1, 1, 1, -1),
-        new Point4D(1, -1, 1, -1),
-        new Point4D(1, 1, -1, -1),
-        new Point4D(1, 1, 1, -1),
-        new Point4D(1, 1, -1, 1),
-        new Point4D(1, -1, 1, 1),
-        new Point4D(-1, 1, 1, 1),
-        new Point4D(1, 1, 1, 1)
-    };
-    private Edge4D[] _edges = new Edge4D[32]
-    {
-        new Edge4D(_vertices[0], _vertices[1]),
-        new Edge4D(_vertices[0], _vertices[2]),
-        new Edge4D(_vertices[0], _vertices[3]),
-        new Edge4D(_vertices[0], _vertices[4]),
-        new Edge4D(_vertices[1], _vertices[5]),
-        new Edge4D(_vertices[1], _vertices[6]),
-        new Edge4D(_vertices[1], _vertices[7]),
-        new Edge4D(_vertices[2], _vertices[5]),
-        new Edge4D(_vertices[2], _vertices[8]),
-        new Edge4D(_vertices[2], _vertices[9]),
-        new Edge4D(_vertices[3], _vertices[6]),
-        new Edge4D(_vertices[3], _vertices[8]),
-        new Edge4D(_vertices[3], _vertices[10]),
-        new Edge4D(_vertices[4], _vertices[7]),
-        new Edge4D(_vertices[4], _vertices[9]),
-        new Edge4D(_vertices[4], _vertices[10]),
-        new Edge4D(_vertices[8], _vertices[11]),
-        new Edge4D(_vertices[9], _vertices[11]),
-        new Edge4D(_vertices[10], _vertices[11]),
-        new Edge4D(_vertices[6], _vertices[12]),
-        new Edge4D(_vertices[7], _vertices[12]),
-        new Edge4D(_vertices[10], _vertices[12]),
-        new Edge4D(_vertices[5], _vertices[13]),
-        new Edge4D(_vertices[7], _vertices[13]),
-        new Edge4D(_vertices[9], _vertices[13]),
-        new Edge4D(_vertices[5], _vertices[14]),
-        new Edge4D(_vertices[6], _vertices[14]),
-        new Edge4D(_vertices[8], _vertices[14]),
-        new Edge4D(_vertices[11], _vertices[15]),
-        new Edge4D(_vertices[12], _vertices[15]),
-        new Edge4D(_vertices[13], _vertices[15]),
-        new Edge4D(_vertices[14], _vertices[15]),
-    };
+    private Coroutine _activeCoroutine = null;
+    private Point4D[] _vertices;
+    private Edge4D[] _edges;
     private float[][] _projectionMatrix = new float[3][]
     {
         new float[4] { 5f, 0f, 0f, 1.5f },
         new float[4] { 0f, 5f, 0f, 1.5f },
         new float[4] { 0f, 0f, 5f, 1.5f }
     };
+    private static float _angle = Mathf.PI * 5f / 180f;
+    private float[][] _rotationMatrix = new float[4][]
+    {
+        new float[4] { Mathf.Cos(_angle), 0f, 0f, -Mathf.Sin(_angle) },
+        new float[4] { 0f, 1f, 0f, 0f},
+        new float[4] { 0f, 0f, 1f, 0f},
+        new float[4] { Mathf.Sin(_angle), 0f, 0f, Mathf.Cos(_angle) },
+    };
     void Start () 
     {
         Hypercube();
+        ActivatorCube.OnInteract += delegate 
+        {
+            if (_activeCoroutine == null)
+                _activeCoroutine = StartCoroutine(Animation());
+            return false; 
+        };
     }
 
     private void Hypercube()
     {
-        _testObject = _vertices.Select(p => {
-            p.Projection(_projectionMatrix);
-            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            go.transform.parent = ModuleObject.transform;
-            go.transform.localPosition = new Vector3(p.ProjectedPoint.X, p.ProjectedPoint.Y, p.ProjectedPoint.Z);
-            go.transform.localScale = new Vector3(1, 1, 1);
-            p.gameObject = go;
-            return go;
-        }).ToArray();
-        _edgeObjects = _edges.Select(e => {
-            GameObject go = Instantiate(CylinderObject);
-            go.transform.parent = ModuleObject.transform;
-            Point3D midpoint = Point3D.Median(e.FirstPoint.ProjectedPoint, e.SecondPoint.ProjectedPoint);
-            go.transform.localPosition = new Vector3(midpoint.X, midpoint.Y, midpoint.Z);
-            go.transform.LookAt(e.FirstPoint.gameObject.transform);
-            go.transform.localScale = new Vector3(0.5f, 0.5f, Point3D.Distance(e.FirstPoint.ProjectedPoint, e.SecondPoint.ProjectedPoint) / 2);
-            return go;
-        }).ToArray();
+        _vertices = new Point4D[16]
+        {
+            new Point4D(-1, -1, -1, -1, HypercubeTransform),
+            new Point4D(-1, -1, -1, 1, HypercubeTransform),
+            new Point4D(-1, -1, 1, -1, HypercubeTransform),
+            new Point4D(-1, 1, -1, -1, HypercubeTransform),
+            new Point4D(1, -1, -1, -1, HypercubeTransform),
+            new Point4D(-1, -1, 1, 1, HypercubeTransform),
+            new Point4D(-1, 1, -1, 1, HypercubeTransform),
+            new Point4D(1, -1, -1, 1, HypercubeTransform),
+            new Point4D(-1, 1, 1, -1, HypercubeTransform),
+            new Point4D(1, -1, 1, -1, HypercubeTransform),
+            new Point4D(1, 1, -1, -1, HypercubeTransform),
+            new Point4D(1, 1, 1, -1, HypercubeTransform),
+            new Point4D(1, 1, -1, 1, HypercubeTransform),
+            new Point4D(1, -1, 1, 1, HypercubeTransform),
+            new Point4D(-1, 1, 1, 1, HypercubeTransform),
+            new Point4D(1, 1, 1, 1, HypercubeTransform)
+        };
+        _edges = new Edge4D[32]
+        {
+            new Edge4D(_vertices[0], _vertices[1], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[0], _vertices[2], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[0], _vertices[3], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[0], _vertices[4], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[1], _vertices[5], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[1], _vertices[6], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[1], _vertices[7], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[2], _vertices[5], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[2], _vertices[8], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[2], _vertices[9], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[3], _vertices[6], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[3], _vertices[8], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[3], _vertices[10], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[4], _vertices[7], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[4], _vertices[9], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[4], _vertices[10], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[8], _vertices[11], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[9], _vertices[11], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[10], _vertices[11], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[6], _vertices[12], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[7], _vertices[12], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[10], _vertices[12], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[5], _vertices[13], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[7], _vertices[13], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[9], _vertices[13], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[5], _vertices[14], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[6], _vertices[14], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[8], _vertices[14], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[11], _vertices[15], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[12], _vertices[15], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[13], _vertices[15], HypercubeTransform, CylinderObject),
+            new Edge4D(_vertices[14], _vertices[15], HypercubeTransform, CylinderObject),
+        };
+        foreach (Point4D p in _vertices)
+            p.Draw(_projectionMatrix);
+        foreach (Edge4D e in _edges)
+            e.Draw();
+    }
+    private IEnumerator Animation()
+    {
+        while (true)
+        {
+            foreach (Point4D p in _vertices)
+            {
+                p.Rotation(_rotationMatrix);
+                p.Draw(_projectionMatrix);
+            }
+            foreach (Edge4D e in _edges)
+                e.Draw();
+            yield return new WaitForSeconds(.05f);
+        }
     }
 }
--- Assets/Point.cs
+++ Assets/Point.cs
@@ -10,16 +10,18 @@ namespace Experiment4
 
         public Point3D ProjectedPoint;
 
-        public GameObject gameObject;
-        public Point4D(float x, float y, float z, float w)
+        public GameObject gameObject = null;
+        public Transform parent = null;
+        public Point4D(float x, float y, float z, float w, Transform parent)
         {
             X = x;
             Y = y;
             Z = z;
             W = w;
+            this.parent = parent;
         }
 
-        public Point4D(float[] coordinate)
+        public Point4D(float[] coordinate, Transform parent)
         {
             if (coordinate == null)
                 throw new ArgumentNullException("Coordinate cannot be null.");
@@ -29,23 +31,50 @@ namespace Experiment4
             Y = coordinate[1];
             Z = coordinate[2];
             W = coordinate[3];
+            this.parent = parent;
         }
 
+        public void Draw(float[][] projectionMatrix)
+        {
+            Projection(projectionMatrix);
+            if (gameObject == null)
+            {
+                gameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                gameObject.transform.parent = parent;
+            }
+            gameObject.transform.localPosition = new Vector3(ProjectedPoint.X, ProjectedPoint.Y, ProjectedPoint.Z);
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
         public void Projection(float[][] projectionMatrix)
         {
             if (projectionMatrix == null)
                 throw new ArgumentNullException("Projection Matrix cannot be null.");
             if (projectionMatrix.Length != 3 || projectionMatrix.Any(p => p.Length != 4))
                 throw new ArgumentException("Projection Matrix must have dimension of 3 by 4.");
-            ProjectedPoint = new Point3D(projectionMatrix[0][0] * X + projectionMatrix[0][1] * Y + projectionMatrix[0][2] * Z + projectionMatrix[0][3] * W,
-                projectionMatrix[1][0] * X + projectionMatrix[1][1] * Y + projectionMatrix[1][2] * Z + projectionMatrix[1][3] * W,
-                projectionMatrix[2][0] * X + projectionMatrix[2][1] * Y + projectionMatrix[2][2] * Z + projectionMatrix[2][3] * W);
+            float[] temp = new float[3];
+            for (int i = 0; i < 3; i++)
+                temp[i] = projectionMatrix[i][0] * X + projectionMatrix[i][1] * Y + projectionMatrix[i][2] * Z + projectionMatrix[i][3] * W;
+            ProjectedPoint = new Point3D(temp[0], temp[1], temp[2]);
+        }
+        public void Rotation(float[][] rotationMatrix)
+        {
+            if (rotationMatrix == null)
+                throw new ArgumentNullException("RotationMatrix Matrix cannot be null.");
+            if (rotationMatrix.Length != 4 || rotationMatrix.Any(p => p.Length != 4))
+                throw new ArgumentException("RotationMatrix Matrix must have dimension of 4 by 4.");
+            float[] temp = new float[4];
+            for (int i = 0; i < 4; i++)
+                temp[i] = rotationMatrix[i][0] * X + rotationMatrix[i][1] * Y + rotationMatrix[i][2] * Z + rotationMatrix[i][3] * W;
+            X = temp[0];
+            Y = temp[1];
+            Z = temp[2];
+            W = temp[3];
         }
     }
     public class Point3D
     {
         public float X, Y, Z;
-
+ 
         public Point3D(float x, float y, float z)
         {
             X = x;
@@ -79,12 +108,30 @@ namespace Experiment4
         public Point4D FirstPoint;
         public Point4D SecondPoint;
 
-        public Edge4D(Point4D firstPoint, Point4D secondPoint)
+        public GameObject Cylinder;
+        public GameObject gameObject = null;
+        public Transform parent;
+        public Edge4D(Point4D firstPoint, Point4D secondPoint, Transform parent, GameObject cylinder)
         {
             if (firstPoint == null || secondPoint == null)
                 throw new ArgumentNullException("Points cannot be null");
             FirstPoint = firstPoint;
             SecondPoint = secondPoint;
+            this.parent = parent;
+            Cylinder = cylinder;
+        }
+
+        public void Draw()
+        {
+            if (gameObject == null)
+            {
+                gameObject = UnityEngine.Object.Instantiate(Cylinder);
+                gameObject.transform.parent = parent;
+            }
+            Point3D midpoint = Point3D.Median(FirstPoint.ProjectedPoint, SecondPoint.ProjectedPoint);
+            gameObject.transform.localPosition = new Vector3(midpoint.X, midpoint.Y, midpoint.Z);
+            gameObject.transform.LookAt(FirstPoint.gameObject.transform);
+            gameObject.transform.localScale = new Vector3(0.5f, 0.5f, Point3D.Distance(FirstPoint.ProjectedPoint, SecondPoint.ProjectedPoint) / 2);
         }
     }
 }
